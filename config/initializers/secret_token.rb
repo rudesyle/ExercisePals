242a53b83388e9a1bf99a9ae2f11314f402b8877
Jeffrey S. Rudesyle <rudesyle@gmail.com>
2012-01-29 10:19:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExercisePals::Application.config.secret_token = '92dbda1329e43d09cbfd30a6a8bfce91264afc6e988984310bc903a5c99d73ff098bf1308cd94828476a5521ebd6cc3d60a7eedcac44de116653402b6dfbc13b'
